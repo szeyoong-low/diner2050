779e76eba7ef9f7f016c70b5bf11806b87bd35a4
szeyoong-low <low.szeyoong@gmail.com>
2026-03-22 13:35:07 +0000

    Frontend: Refactored menu grid

--- frontend/components/cards/menu-grid.tsx
+++ frontend/components/cards/menu-grid.tsx
@@ -8,10 +8,19 @@ interface ISummariesGridProps {
 }
 
 export function MenuGrid({ menuItems, className }: ISummariesGridProps) {
+  const menuByCategory = Map.groupBy(menuItems, (item) => item.Category);
+  console.log(menuByCategory);
+
   return (
-    <div className={cn("grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6", className)}>
-      {menuItems.map((item: TMenuItem) => (
-        <MenuCard key={item.documentId} menuItem={item} />
+    <div className={cn("flex flex-col gap-6", className)}>
+      {[...menuByCategory.entries()].map(([category, items]) => (
+        <section key={category}>
+          <h2 className="font-extrabold mb-3 text-2xl">{category}</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {items.map((item: TMenuItem) => (
+            <MenuCard key={item.documentId} menuItem={item} />))}
+          </div>
+        </section>
       ))}
     </div>
   );
