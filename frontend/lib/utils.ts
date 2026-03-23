fa8ff56ecb4eef235c34614cc82da6740d1a5190
szeyoong-low <low.szeyoong@gmail.com>
2026-03-23 20:26:31 +0000

    Frontend: Changes made for deployment

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -7,5 +7,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getStrapiURL() {
-  return process.env.NEXT_PUBLIC_STRAPI_URL ?? "http://localhost:1337";
+  return process.env.NEXT_PUBLIC_STRAPI_URL;
 }
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,10 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**/*",
+        protocol: "https",
+        hostname: "healing-comfort-c04cbb783b.media.strapiapp.com",
       },
     ],
   },
